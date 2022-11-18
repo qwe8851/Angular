62f5b859304d508100d5ac3d134ae56ac9c8a5f4
qwe8851 <dgh07027@naver.com>
2022-11-19 01:03:37 +0900

    input.value를 콘솔창에 출력하는 함수 생성

--- angular-raddit/src/app/app.component.ts
+++ angular-raddit/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-raddit';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement):boolean{
+    console.log(`Adding article title : ${title.value}, link : ${link.value}`);
+    return false;
+  };
 }
