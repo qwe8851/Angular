cd506656d1343f353160ae5e2639e20f6288ec83
qwe8851 <dgh07027@naver.com>
2022-11-20 18:48:29 +0900

    article.components 캡슐화 하기

--- angular-raddit/src/app/article/article.component.ts
+++ angular-raddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -6,24 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-  title: string;
-  link: string;
-  votes: number;
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article:Article;
+  // title: string; //원래 있던 변수들은 article class의 변수들로 대체
+  // link: string;
+  // votes: number;
 
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    // 게시물은 Input 행에 위치
+    this.article = new Article(
+      'Angular 2', 
+      'http://angular.io', 
+      10);
+    // this.title = 'Angular 2';  //article class로 값을 전송
+    // this.link = 'http://angular.io';
+    // this.votes = 10;
   }
 
   voteUp(): boolean {
-    this.votes += 1;
-    return false;
+    this.article.voteUp();
+    // this.votes += 1; //여기서 직접 함수를 수정하지않고 캡슐화하였다.
+    return false; //재랜더링을 막음
   }
 
-  voteDown(): boolean  {
-    this.votes -= 1;
-    return false;
+  voteDown(): boolean {
+    this.article.voteDown();
+    // this.votes -= 1;
+    return false; //재랜더링을 막음
   }
 
   ngOnInit() {}
---
+++ angular-raddit/src/app/article/article.model.ts
@@ -0,0 +1,32 @@
+import { DomSanitizer } from '@angular/platform-browser';
+
+export class Article {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes += 1;
+  }
+
+  //   domain()은 URL에서 도메인을 추출해내는 유틸리티 함수임
+  domain(): string|null {
+    try {
+      // ex: http://foo.com/path.bar
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (err) {
+      return null;
+    }
+  }
+}
