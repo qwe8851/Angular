af4dea9c0487d3f795b91a7ca3aa506779f32fdc
qwe8851 <dgh07027@naver.com>
2022-11-27 21:01:13 +0900

    [과제] success/warning 컴포넌트 직접생성& node menager로 생성 해보기

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
