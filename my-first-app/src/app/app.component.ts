4f5f8a945f8f945029081dbf1c6c976880f6d17c
DESKTOP-BB48D0V\se <dgh07027@naver.com>
2022-11-28 13:59:55 +0900

    ngFor index 사용

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // username:string;
   showSecret: boolean;
-  log: number[];
+  log: number[] | any;
 
   constructor() {
     // this.username= '';
